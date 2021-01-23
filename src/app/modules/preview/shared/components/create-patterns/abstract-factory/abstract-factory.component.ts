d45176ec19ca8e08d9aa57702fdd3566091be01f
kochutyu <kochutyurawork@gmail.com>
2021-01-24 00:58:12 +0200

    Added > abstract factory [done]

--- src/app/modules/preview/shared/components/create-patterns/abstract-factory/abstract-factory.component.ts
+++ src/app/modules/preview/shared/components/create-patterns/abstract-factory/abstract-factory.component.ts
@@ -1,11 +1,43 @@
-import {Component} from "@angular/core";
+import {ChangeDetectionStrategy, Component} from "@angular/core";
+import {TeslaService} from "./shared/services/tesla.service";
+import {ETeslaSpecification} from "./shared/enums/tesla-specification.enum";
+import {ETeslaModel} from "./shared/enums/tesla-model.enum";
+import {TeslaConstant} from "./shared/constant/tesla.constant";
 
 
 @Component({
-  template: `
-    Abstract Method
-  `
+  styleUrls: ['./abstract-factory.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './abstract-factory.component.html'
 })
 export class AbstractFactoryComponent {
+
+  public models = TeslaConstant;
+
+  constructor(
+    public tesla: TeslaService
+  ) {
+    this.tesla.factory(ETeslaSpecification.SPECIFICATION_1);
+    this.tesla.factory(ETeslaSpecification.SPECIFICATION_2);
+  }
+
+  public crateModel(factory: number, index: number): void {
+    const currentFactory = this.tesla.getFactories[factory];
+    switch (index) {
+      case 0:
+        currentFactory.createCar(ETeslaModel.MODEL_S)
+        break;
+      case 1:
+        currentFactory.createCar(ETeslaModel.MODEL_X)
+        break;
+      case 2:
+        currentFactory.createCar(ETeslaModel.MODEL_3)
+        break;
+    }
+  }
+
+  factoryBy(index, item) {
+    return item.specification;
+  }
 
 }
---
+++ src/app/modules/preview/shared/components/create-patterns/abstract-factory/shared/enums/tesla-model.enum.ts
@@ -0,0 +1,5 @@
+export enum ETeslaModel {
+  MODEL_S = 'Model_S',
+  MODEL_X = 'Model_X',
+  MODEL_3 = 'Model_3'
+}
